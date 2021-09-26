f93b2e79f345da9f6615401229cabe35f4685890
CatJelli <catjelli@gmail.com>
2021-09-26 18:47:16 +0300

    Выполнила module2-task1

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const getRandomIntFromRange = function (min, max) {
+  if (max > min && max >= 0 && min >= 0) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {
+    return console.log ('Неверный аргумент');
+  }
+};
+//Источник: https://myrusakov.ru/js-random-numbers.html
+
+const getRandomFromRange = function (min, max, digits) {
+  if (max > min && max >= 0 && min >= 0 && digits >= 0) {
+    let randomFromRange = Math.random() * (max - min) + min;
+    return +randomFromRange.toFixed(digits);
+  } else {
+    return console.log ('Неверный аргумент');
+  }
+};
+//Источник: https://learn.javascript.ru/number#okruglenie
