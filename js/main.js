051caa0fd3e26bcfffdb17ccbe7a4933e1d581e5
CatJelli <catjelli@gmail.com>
2021-09-26 19:05:08 +0300

    Исправила ошибки, на которые ругался линтер (:

--- js/main.js
+++ js/main.js
@@ -2,15 +2,19 @@ const getRandomIntFromRange = function (min, max) {
   if (max > min && max >= 0 && min >= 0) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  return console.log ('Неверный аргумент');
+  return 'Неверный аргумент';
 };
 //Источник: https://myrusakov.ru/js-random-numbers.html
 
 const getRandomFromRange = function (min, max, digits) {
   if (max > min && max >= 0 && min >= 0 && digits >= 0) {
-    let randomFromRange = Math.random() * (max - min) + min;
+    const randomFromRange = Math.random() * (max - min) + min;
     return +randomFromRange.toFixed(digits);
   }
-  return console.log ('Неверный аргумент');
+  return 'Неверный аргумент';
 };
 //Источник: https://learn.javascript.ru/number#okruglenie
+
+getRandomFromRange(1, 57, 5);
+
+getRandomIntFromRange(57, 79);
